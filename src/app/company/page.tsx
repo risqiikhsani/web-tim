dc541a2628906d74e667d9c569b805e9d2ff80c1
risqiikhsani <risqiikhsani16@gmail.com>
2024-12-03 16:35:35 +0700

    added cta to company profile

--- src/app/company/page.tsx
+++ src/app/company/page.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import HeroAwsPartner from '../_public_components/hero-aws-partner'
 import MapboxLocationViewer from '../_public_components/map'
 import Contact from '../_public_components/contact'
+import CTA from '../_page/cta'
 
 export default function page() {
   return (
@@ -12,6 +13,7 @@ export default function page() {
       <P className='text-muted-foreground'>PT TIM was established in 2010, and since 2021 we have added core business to provide cloud computing infrastructure services (consultancy, design, migration, implementation and operation support) and delivering AWS training and certification. PT TIM has become an official member of AWS Partner Network in Indonesia since 2022.</P>
       <HeroAwsPartner/>
       <MapboxLocationViewer latitude={-6.259741690596165} longitude={106.78175217951646}/>
+      <CTA/>
       <Contact/>
     </div>
   )
